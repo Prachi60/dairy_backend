c281df86c66d54ba873f52cfcaf1788e86076a67
Prachi Porwal <prachiporwal60@gmail.com>
2025-12-23 15:44:16 +0530

    cors enabled.

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-import express from "express"
-import dotenv from  "dotenv"
-import connectDB  from "./app/dbConfig/dbConfig.js"
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./app/dbConfig/dbConfig.js";
 import setupRoutes from "./app/routes/index.js";
-import mediaSetup from "./app/routes/media.js"
+import mediaSetup from "./app/routes/media.js";
 import path from "path";
-import cors  from "cors"
+import cors from "cors";
 
 dotenv.config();
-const app= express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOSTNAME || "0.0.0.0";
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["*"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 connectDB();
 setupRoutes(app);
 mediaSetup(app);
 app.use("/media", express.static(path.join(process.cwd(), "uploads")));
 
-
-app.listen(PORT,HOST,()=>{
-    console.log(`server is running on http://${HOST}:${PORT}`)
-})
+app.listen(PORT, HOST, () => {
+  console.log(`server is running on http://${HOST}:${PORT}`);
+});
