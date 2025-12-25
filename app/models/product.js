97661b0e4900630e0339e6da272d76a10bf8bee8
Prachi Porwal <prachiporwal60@gmail.com>
2025-12-25 14:11:13 +0530

    product changed

--- app/models/product.js
+++ app/models/product.js
@@ -5,16 +5,16 @@ const productSchema =new mongoose.Schema({
     name:{type:String,required:true},
     category:{type:mongoose.Schema.Types.ObjectId,ref:"Category",required:true},
     price:{type:Number,required:true},
-    unit:{type:String,required:true},
+    unit:{type:String},
     description:{type:String},
     // store:{type:mongoose.Schema.Types.ObjectId,ref:"Store",required:true},
     store:{type:String},
     in_stock:{type:Boolean,required:true},
-    subscription_available:{type:Boolean,default:true},
+    subscription_available:{type:Boolean},
     expire_date:{type:String,required:true},
-    subscription_frequency:{type:String,enum:["daily","alternate_day","weekend_only","weekly"],default:"daily"},
+    // subscription_frequency:{type:String,enum:["daily","alternate_day","weekend_only","weekly"],default:"daily"},
     image:{type:[String]},
-    max_order_limit:{type:Number,default:5}
+    // max_order_limit:{type:Number,default:5}
 
 },
 {timestamps:true}
